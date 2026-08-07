66f0063009c52772498bf557552e06ee7cccec05
Yamrc <yamrci@outlook.com>
2026-08-07 16:37:34 +0800

    Feat: 项目初始化

---
+++ src/main/java/cc/yamrc/konechat/Main.java
@@ -0,0 +1,14 @@
+package cc.yamrc.konechat;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(Main.MODID)
+public class Main {
+    public static final String MODID = "konechat";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+    }
+}
